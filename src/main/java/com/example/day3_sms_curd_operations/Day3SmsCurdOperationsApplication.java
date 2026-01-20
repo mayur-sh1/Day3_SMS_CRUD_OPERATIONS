2d2c5bec6f1372af26e385a341206ea7b8fa4df7
mayur-sh1 <mayursharma20000@gmail.com>
2026-01-20 11:55:34 +0530

    Initialized Git Repository

---
+++ src/main/java/com/example/day3_sms_curd_operations/Day3SmsCurdOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3_sms_curd_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3SmsCurdOperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3SmsCurdOperationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day3_sms_curd_operations/Day3SmsCurdOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3_sms_curd_operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3SmsCurdOperationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
